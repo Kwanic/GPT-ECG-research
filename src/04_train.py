5b1286d47a8098cdad2d43ee974c5cb7d00cca54
Caipingg <kuanqingdao@gmail.com>
2025-09-04 14:30:17 -0500

    ktest

--- src/04_train.py
+++ src/04_train.py
@@ -1,5 +1,5 @@
 import xgboost as xgb
-
+#123123
 def train_xgb(X_tr, y_tr, X_va, y_va, params):
     dtr, dva = xgb.DMatrix(X_tr, label=y_tr), xgb.DMatrix(X_va, label=y_va)
     booster = xgb.train(params, dtr, num_boost_round=200,
